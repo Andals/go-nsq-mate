5d1e1379989447d18377674b9e662eb7c178e607
ligang <ligang1109@aliyun.com>
2017-12-11 16:42:09 +0800

    add consumer_task

--- consumer_test.go
+++ consumer_test.go
@@ -1,22 +1,50 @@
 package nsqmate
 
 import (
+	"github.com/andals/golog"
+
 	"fmt"
 	"testing"
 	"time"
 )
 
-func TestConf(t *testing.T) {
+func newTestConsumer() *Consumer {
 	config := NewConfig()
 	config.MaxInFlight = 100
 	config.LookupdPollInterval = time.Second * 3
 
-	consumer, _ := NewConsumer("app3_t3", "c1", config)
+	consumer, _ := NewConsumer("go-nsq-mate-dev", "c1", config)
 	consumer.AddLookupd("127.0.0.1:4161").
-		SetMsgProcessor(NewGzipMessageProcessor(nil)).
-		SetHandleFunc(func(message *Message) error {
-			fmt.Println("Recieve", string(message.Body))
-			return nil
-		})
+		SetMsgProcessor(NewGzipMessageProcessor(nil))
+
+	return consumer
+}
+
+func TestConsumer(t *testing.T) {
+	consumer := newTestConsumer()
+	consumer.SetHandleFunc(func(message *Message) error {
+		fmt.Println("Recieve", string(message.Body))
+		return nil
+	})
+
 	consumer.Run()
+}
+
+type ConsumerTaskDemo struct {
+	*BaseConsumerTask
+}
+
+func TestConsumerTask(t *testing.T) {
+	fw, _ := golog.NewFileWriter("/tmp/demo_consumer_task.log")
+	logger, _ := golog.NewSimpleLogger(fw, golog.LEVEL_DEBUG, golog.NewSimpleFormater())
+	ct := NewBaseConsumerTask("DemoConsumerTask").
+		SetLogger(logger).
+		SetConsumer(newTestConsumer()).
+		SetLineWorker(10, demoLineProcessFunc)
+
+	ct.Start()
+}
+
+func demoLineProcessFunc(line []byte, workerId int) {
+	fmt.Println("WorkerId:", workerId, "line:", string(line))
 }
